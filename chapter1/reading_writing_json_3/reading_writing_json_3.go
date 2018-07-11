45896175d2c0bcb6fc6bf798cd0d1ec93c32ac2e
Apipol Sukgler <apipol@computerlogy.com>
2018-07-11 13:59:44 +0700

    [Chapter 1] <Step 5> change from Marshal to Encoder

---
+++ chapter1/reading_writing_json_3/reading_writing_json_3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	// change the output field to be "message"
+	Message string `json:"message"`
+	// do not output this field
+	Author string `json:"-"`
+	// do not output the field if the value is empty
+	Date string `json:",omitempty"`
+	// convert output to a string and rename "id"
+	Id int `json:"id,string"`
+}
+
+func main() {
+	port := 8000
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+	log.Printf("Server starting on port %v", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "Hello world"}
+	encoder := json.NewEncoder(w)
+	encoder.Encode(response)
+}
