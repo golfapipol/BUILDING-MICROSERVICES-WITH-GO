24376d8506959d44b3bd36f6126c9adb8f16f65b
Apipol Sukgler <apipol@computerlogy.com>
2018-07-11 13:33:12 +0700

    [Chapter 1] <Step 2> return json via using struct in helloWorldHandler

---
+++ chapter1/reading_writing_json_1/reading_writing_json_1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type helloWorldResponse struct {
+	Message string
+}
+
+func main() {
+	port := 8000
+
+	http.HandleFunc("/helloworld", helloWorldHandler)
+	log.Printf("Server starting on port %v", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
+}
+
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	response := helloWorldResponse{Message: "Hello world"}
+	data, err := json.Marshal(response)
+	if err != nil {
+		panic("Opps")
+	}
+	fmt.Fprint(w, string(data))
+}
